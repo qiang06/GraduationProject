1cf7859d757b8878e416c0aeff58a6f2c09641ce
qiang06 <hxq061008@gmail.com>
2025-06-08 20:08:16 +0800

    feat: 添加IDEA项目配置文件及忽略规则

---
+++ my-project-backen/src/main/java/com/myproject/MyProjectBackenApplication.java
@@ -0,0 +1,13 @@
+package com.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectBackenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectBackenApplication.class, args);
+	}
+
+}
